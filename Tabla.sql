773d6adab268f0776865a7053980c35ed6431dc3
Carlos Navarrete <carlos.flores0388@alumnos.udg.mx>
2024-11-26 19:49:05 -0600

    s

--- Tabla.sql
+++ Tabla.sql
@@ -1,17 +1,48 @@
-CREATE TABLE Usuarios (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    Nombres VARCHAR(100) NOT NULL,
-    Apellidos VARCHAR(100) NOT NULL,
-    CorreoE VARCHAR(150) UNIQUE NOT NULL,
-    Codigo VARCHAR(9) UNIQUE NOT NULL,
-    Contrasena VARCHAR(255) NOT NULL,
-    FechaNacimiento DATE NOT NULL,
-    Carrera VARCHAR(100) NOT NULL
+-- Crear la tabla Lugar
+CREATE TABLE Lugar (
+    lugar_id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100),
+    calle VARCHAR(100),
+    no_exterior INT,
+    tipo_establecimiento VARCHAR(50),
+    descripcion TEXT,
+    estrellas_prom DECIMAL(3,2)
 );
 
-
--- El insert para la base de datos
-
-INSERT INTO Usuarios (Nombres, Apellidos, CorreoE, Contrasena, Codigo, FechaNacimiento, Carrera) 
-VALUES ('Nombre', 'Apellidos', 'CorreoE', 'Contrasena', 'Codigo', '2001-10-25', 'Carrera');
-
+-- Crear la tabla Usuario
+CREATE TABLE Usuario (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    apellidos VARCHAR(100),
+    password VARCHAR(255),
+    codigo INT,
+    fecha_nacimiento DATE,
+    carrera INT
+);
+-- Crear la tabla Lugar_likes
+CREATE TABLE Lugar_Likes (
+    lugar_id INT,
+    user_id INT,
+    like BOOLEAN,
+    estrellas INT,
+    PRIMARY KEY (lugar_id, user_id),
+    FOREIGN KEY (lugar_id) REFERENCES Lugar(lugar_id),
+    FOREIGN KEY (user_id) REFERENCES Usuario(user_id)
+);
+-- Crear la tabla Comentario
+CREATE TABLE Comentario (
+    comentario_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    lugar_id INT,
+    comentario TEXT,
+    FOREIGN KEY (user_id) REFERENCES Usuario(user_id),
+    FOREIGN KEY (lugar_id) REFERENCES Lugar(lugar_id)
+);
+-- Crear la tabla Comentario_like
+CREATE TABLE Comentario_Like (
+    comentario_id INT,
+    user_id INT,
+    PRIMARY KEY (comentario_id, user_id),
+    FOREIGN KEY (comentario_id) REFERENCES Comentario(comentario_id),
+    FOREIGN KEY (user_id) REFERENCES Usuario(user_id)
+);
