d3b89d454674eca6c05b83844df27ecd93b50ed6
Carlos Navarrete <carlos.flores0388@alumnos.udg.mx>
2024-11-27 14:56:52 -0600

    post comentso

--- valida.php
+++ valida.php
@@ -4,36 +4,31 @@ require "/home/conectared.php";
 $con = conecta();
 $correo=$_POST['correo'];
 $password=$_REQUEST['pass'];
-$pass=md5($password);
+#$pass=md5($password);
 $pass=$password;
 
 
 // Consulta para verificar el usuario
-$sql = "SELECT * FROM Usuario WHERE correo = '$correo' AND pass = '$pass' AND status = 1 AND eliminado = 0";
+$sql = "SELECT * FROM Usuario WHERE correo = '$correo' AND password = '$pass'";
 $res = $con->query($sql);
 
 if ($res->num_rows == 0) {
-    $a = 0;
-} else {
-$row = $res->fetch_array();
-    $a = 1;
-    $id = $row["id"];
-
-    // Guarda datos del usuario en la sesión
-    $_SESSION['idu'] = $id;
-
-// Si existe un pedido, cargar los productos en el carrito
-if (isset($_SESSION['idp'])) {
-    $idp = $_SESSION['idp'];
-    $sqlProductos = "SELECT id_producto, cantidad FROM pedidos_poductos WHERE id_pedido = '$idp'";
-    $resProductos = $con->query($sqlProductos);
-
-    while ($producto = $resProductos->fetch_assoc()) {
-        $_SESSION['carrito'][$producto['id_producto']] = $producto['cantidad'];
+    $a=0;
     }
-}
-}
+    else{
+        $row=$res->fetch_array();
+        $rol =$row["rol"];
+        $a=1;
+        $id =$row["id"];
+        $nombre =$row["nombre"];
+        $apellidos =$row["apellidos"];
+        $_SESSION['id']=$id;
+        $_SESSION['nombre']=$nombre;
+        $_SESSION['apellidos']=$apellidos;
+        $_SESSION['correo']=$correo;
+        $_SESSION['rol']=$rol_txt;
 
-
-echo $a;
+    }
+    echo $a;
+    
 ?>
