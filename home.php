20218d1826a3d1839cdb5ad578f1b1b7562225b1
Carlos Navarrete <carlos.flores0388@alumnos.udg.mx>
2024-11-27 00:56:09 -0600

    home

--- home.php
+++ home.php
@@ -1,3 +1,14 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require "/home/conectared.php";
+$con = conecta();
+#if(empty($_SESSION['id'])){
+#    header('Location: ./login.php');
+#    }
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
