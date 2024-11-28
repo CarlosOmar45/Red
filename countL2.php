93e0ba6fac490069ab94b173c515684358d517bd
Carlos Navarrete <carlos.flores0388@alumnos.udg.mx>
2024-11-27 23:19:01 -0600

    con25112a122111q12

--- countL2.php
+++ countL2.php
@@ -23,7 +23,7 @@ $item_type = 2;
 
 // Verificar si ya existe un like
 $sql = $con->prepare("SELECT like_value, like_id FROM Likes WHERE user_id = ? AND item_type = ? AND comentario_id = ? AND lugar_id = ?");
-$sql->bind_param("isii", $user_id, $item_type, $id, $lugar_id);
+$sql->bind_param("iiii", $user_id, $item_type, $id, $lugar_id);
 $sql->execute();
 $res = $sql->get_result();
 
@@ -31,7 +31,7 @@ if ($res->num_rows === 0) {
     // Insertar nuevo like
     $like_value = 1;
     $sql = $con->prepare("INSERT INTO Likes (user_id, comentario_id, item_type, like_value, lugar_id) VALUES (?, ?, ?, ?, ?)");
-    $sql->bind_param("iisii", $user_id, $id, $item_type, $like_value, $lugar_id);
+    $sql->bind_param("iiiii", $user_id, $id, $item_type, $like_value, $lugar_id);
     $response['success'] = $sql->execute();
 } else {
     // Alternar valor de like
@@ -45,7 +45,7 @@ if ($res->num_rows === 0) {
 
 // Obtener el total de likes
 $sql = $con->prepare("SELECT COUNT(*) AS total_likes FROM Likes WHERE comentario_id = ? AND item_type = ? AND like_value = 1");
-$sql->bind_param("is", $id, $item_type);
+$sql->bind_param("ii", $id, $item_type);
 $sql->execute();
 $res = $sql->get_result();
 if ($row = $res->fetch_assoc()) {
