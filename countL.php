47ae75cc90817f141a2be212576f837adb2ab318
Carlos Navarrete <carlos.flores0388@alumnos.udg.mx>
2024-11-27 20:52:18 -0600

    post careaaa3

--- countL.php
+++ countL.php
@@ -5,36 +5,9 @@ $con = conecta();
 $tipo = $_POST['tipo'];
 $id = $_POST['id'];
 $user_id = $_SESSION['id'];
+$response = array('success' => false, 'total_likes' => 0);
 
-if ($tipo == 1) {
-    $sql = $con->prepare("SELECT like_value, like_id FROM Likes WHERE user_id = ? AND item_type = ? AND lugar_id = ?");
-    $sql->bind_param("iii", $user_id, $tipo, $id);
-    $sql->execute();
-    $res = $sql->get_result();
-
-    if ($res->num_rows == 0) {
-        $like = TRUE;
-        $sql = $con->prepare("INSERT INTO Likes (user_id, lugar_id, item_type, like_value) VALUES (?, ?, ?, ?)");
-        $sql->bind_param("iiii", $user_id, $id, $tipo, $like);
-        if ($sql->execute()) {
-            echo "Registro exitoso";
-        } else {
-            echo "Error en el registro: " . $sql->error;
-        }
-    } else {
-        $row = $res->fetch_array();
-        $like = $row["like_value"];
-        $like_id = $row["like_id"];
-        $sql = $con->prepare("UPDATE Likes SET like_value = ? WHERE like_id = ?");
-        $like = !$like;
-        $sql->bind_param("ii", $like, $like_id);
-        if ($sql->execute()) {
-            echo "Actualización exitosa";
-        } else {
-            echo "Error en la actualización: " . $sql->error;
-        }
-    }
-} else {
+if ($tipo == 2) {
     $sql = $con->prepare("SELECT like_value, like_id FROM Likes WHERE user_id = ? AND item_type = ? AND comentario_id = ?");
     $sql->bind_param("iii", $user_id, $tipo, $id);
     $sql->execute();
@@ -45,9 +18,9 @@ if ($tipo == 1) {
         $sql = $con->prepare("INSERT INTO Likes (user_id, comentario_id, item_type, like_value) VALUES (?, ?, ?, ?)");
         $sql->bind_param("iiii", $user_id, $id, $tipo, $like);
         if ($sql->execute()) {
-            echo "Registro exitoso";
+            $response['success'] = true;
         } else {
-            echo "Error en el registro: " . $sql->error;
+            $response['success'] = false;
         }
     } else {
         $row = $res->fetch_array();
@@ -57,12 +30,22 @@ if ($tipo == 1) {
         $like = !$like;
         $sql->bind_param("ii", $like, $like_id);
         if ($sql->execute()) {
-            echo "Actualización exitosa";
+            $response['success'] = true;
         } else {
-            echo "Error en la actualización: " . $sql->error;
+            $response['success'] = false;
         }
     }
+
+    // Obtener el total de likes
+    $sql = $con->prepare("SELECT COUNT(*) AS total_likes FROM Likes WHERE comentario_id = ? AND item_type = ? AND like_value = TRUE");
+    $sql->bind_param("ii", $id, $tipo);
+    $sql->execute();
+    $res = $sql->get_result();
+    $row = $res->fetch_array();
+    $response['total_likes'] = $row['total_likes'];
 }
 
+echo json_encode($response);
 $con->close();
 ?>
+
